d85993d096d0aa8fed06deef9f4dcda0aa103679
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-27 16:31:01 +0700

    refactor: added catch (Main)

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,9 @@ import { api } from "../utils/Api";
 import Card from "./Card";
 
 function Main(props) {
-    const [userName, setUserName] = React.useState();
-    const [userDescription, setUserDescription] = React.useState();
-    const [userAvatar, setUserAvatar] = React.useState();
+    const [userName, setUserName] = React.useState('');
+    const [userDescription, setUserDescription] = React.useState('');
+    const [userAvatar, setUserAvatar] = React.useState('');
     const [cards, setCards] = React.useState([]);
 
     React.useEffect(() => {
@@ -16,11 +16,17 @@ function Main(props) {
                 setUserDescription(res.about);
                 setUserName(res.name);
             })
+            .catch((error) => {
+                console.log('error: ', error);
+            })
 
         api.getCards()
             .then(((cards) => {
                 setCards(cards);
             }))
+            .catch((error) => {
+                console.log('error: ', error);
+            })
     })
 
     return (
